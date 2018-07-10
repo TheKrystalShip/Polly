14a468474c66716997a95c09405410e56b81a4e6
Cristian Moraru <cristian.moraru@live.com>
2018-07-10 20:42:02 +0200

    Add project files.

---
+++ BotTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BotTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
